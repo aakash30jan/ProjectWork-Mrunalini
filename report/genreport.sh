626dcd8fff5eb3d3c3c4410d1c7d68138116d19f
aero <aakash30jan@gmail.com>
2019-01-08 21:55:04 +0100

    seminar report structure mod1

--- report/genreport.sh
+++ report/genreport.sh
@@ -1,6 +1,9 @@
 pdflatex -interaction=nonstopmode frontpages.tex 
 pdflatex -interaction=nonstopmode document.tex 
 
+pdflatex -interaction=nonstopmode frontpages.tex 
+pdflatex -interaction=nonstopmode document.tex 
+
 pdftk frontpages.pdf document.pdf cat output PatilMrunalini_Report.pdf
  
 xdg-open PatilMrunalini_Report.pdf
